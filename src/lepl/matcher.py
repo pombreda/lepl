be213342c10d04c5baa294e56c92be4b394d9a22
andrew <andrew@localhost>
2009-02-07 12:19:11 +0000

    graph repr

--- src/lepl/matcher.py
+++ src/lepl/matcher.py
@@ -35,12 +35,23 @@ class NodeMixin(object):
         return self.__walker(visitor)
         
     def __repr__(self):
-        visitor = MatcherStr()
+        visitor = GraphStr()
         lines = self.walk(visitor)
         return visitor.postprocess(lines)
 
 
 class Visitor(object):
+    '''
+    The interface required by the Walker.
+    
+    'loop' is value returned when a node is re-visited.
+    
+    'arg' is called for children that are not NodeMixin instances.
+    
+    'type_' is set with the node type before node() is called.  This
+    allows node() itself to be invoked with the Python arguments used to
+    construct the original graph.
+    '''
     
     def __init__(self):
         self.type_ = None
@@ -54,6 +65,17 @@ class Visitor(object):
 
 
 class Walker(object):
+    '''
+    Depth first tree walker.
+    
+    Some processes require bottom-up and then top-down processing.  This can
+    be achieved by applying this walker (bottom-up) to return functions which
+    are then evaluated top-down.  See the GraphStr visitor for an example.
+    
+    This is based directly on the catamorphism of the graph.  The visitor 
+    encodes the type information.  Some of the code is ugly because of the
+    way arguments are packed in Python-style args/kargs.
+    '''
     
     def __init__(self, root):
         self.__root = root
@@ -109,11 +131,12 @@ class Walker(object):
             return visitor.arg(node)
         
                 
-class MatcherStr(Visitor):
+class ConstructorStr(Visitor):
     
-    def __init__(self):
+    def __init__(self, line_length=80):
         super(MatcherStr, self).__init__()
         self.loop = [[0, '<loop>']]
+        self.__line_length = line_length
     
     def node(self, *args, **kargs):
         contents = []
@@ -162,7 +185,8 @@ class MatcherStr(Visitor):
             if indent == lines[start][0] and \
                     (start+1 >= stop or indent == lines[start+1][0]) and \
                     (start+2 >= stop or indent == lines[start+2][0]) and \
-                    indent + len(lines[start][1]) + len(lines[start+1][1]) < 80:
+                    indent + len(lines[start][1]) + len(lines[start+1][1]) < \
+                        self.__line_length:
                 lines[start][1] += lines[start+1][1]
                 del lines[start+1]
                 stop -= 1
@@ -175,7 +199,7 @@ class MatcherStr(Visitor):
         size = indent + len(text) 
         for (_, extra) in lines[start:stop]:
             size += len(extra)
-            if size > 80:
+            if size > self.__line_length:
                 raise Exception('too long')
             text += extra
         lines[start-1] = [indent, text]
@@ -185,4 +209,33 @@ class MatcherStr(Visitor):
     def __format(self, lines):
         return '\n'.join(' ' * indent + line for (indent, line) in lines)
                 
+                
+class GraphStr(Visitor):
+    
+    def __init__(self):
+        super(GraphStr, self).__init__()
+        self.loop = lambda first, rest, name: [first + name + ' <loop>']
+    
+    def node(self, *args, **kargs):
+        def fun(first, rest, name, type_=self.type_):
+            spec = []
+            for arg in args:
+                spec.append(('+- ', '|  ', '', arg))
+            for arg in kargs:
+                spec.append(('+- ', '|  ', arg, kargs[arg]))
+            if spec:
+                spec[-1] = ('`- ', '   ', spec[-1][2], spec[-1][3])
+            yield first + name + (' ' if name else '') + type_.__name__
+            for (a, b, c, f) in spec:
+                for line in f(a, b, c):
+                    yield rest + line
+        return fun
+    
+    def arg(self, value):
+        return lambda first, rest, name: [first + name + ' ' + repr(value)]
+    
+    def postprocess(self, f):
+        return '\n'.join(f('', '', ''))
+    
+
         
