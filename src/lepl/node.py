4a5f7a65de61db82cb48b92199fc1bd4e554edbe
andrew <andrew@small.europa2189>
2010-03-23 21:48:55 -0400

    initial fix for named node issue

--- src/lepl/node.py
+++ src/lepl/node.py
@@ -32,6 +32,36 @@ def is_named(arg):
             and len(arg) == 2 and isinstance(arg[0], basestring)
             
 
+class NodeException(Exception):
+    '''
+    Exception raised when we have problems dynamically creating nodes.
+    '''
+            
+            
+def new_named_node(name, node):
+    '''
+    Generate a sub-class of Node, with the given name as type, as long as
+    it is not already a subclass.
+    '''
+    if type(node) != Node:
+        raise NodeException(
+            format('Will not coerce a node subclass ({0}) to {1}',
+                   type(node), name))
+    class_ = type(name, (Node,), {})
+    (args, kargs) = node._constructor_args()
+    return class_(*args, **kargs)
+
+
+def coerce(arg):
+    '''
+    Convert named nodes to nodes with that name.
+    '''
+    if is_named(arg) and isinstance(arg[1], Node):
+        return new_named_node(arg[0], arg[1])
+    else:
+        return arg
+    
+
 # pylint: disable-msg=R0903
 # it's not supposed to have public attributes, because it exposes contents
 class Node(LogMixin, ConstructorGraphNode):
@@ -51,7 +81,7 @@ class Node(LogMixin, ConstructorGraphNode):
         self.__children = []
         self.__paths = []
         self.__names = set()
-        for arg in args:
+        for arg in map(coerce, args):
             if is_named(arg):
                 self.__add_named_child(arg[0], arg[1])
             elif isinstance(arg, Node):
