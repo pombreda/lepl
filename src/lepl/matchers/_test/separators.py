5b6a79bde6cadd10b5f0dae3d07b03f55e969545
andrew <andrew@small.europa2189>
2010-04-02 16:47:28 -0400

    change licence in source

--- src/lepl/matchers/_test/separators.py
+++ src/lepl/matchers/_test/separators.py
@@ -1,20 +1,31 @@
 
-# Copyright 2009 Andrew Cooke
-
-# This file is part of LEPL.
-# 
-#     LEPL is free software: you can redistribute it and/or modify
-#     it under the terms of the GNU Lesser General Public License as published 
-#     by the Free Software Foundation, either version 3 of the License, or
-#     (at your option) any later version.
-# 
-#     LEPL is distributed in the hope that it will be useful,
-#     but WITHOUT ANY WARRANTY; without even the implied warranty of
-#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#     GNU Lesser General Public License for more details.
-# 
-#     You should have received a copy of the GNU Lesser General Public License
-#     along with LEPL.  If not, see <http://www.gnu.org/licenses/>.
+# The contents of this file are subject to the Mozilla Public License
+# (MPL) Version 1.1 (the "License"); you may not use this file except
+# in compliance with the License. You may obtain a copy of the License
+# at http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS"
+# basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+# the License for the specific language governing rights and
+# limitations under the License.
+#
+# The Original Code is LEPL (http://www.acooke.org/lepl)
+# The Initial Developer of the Original Code is Andrew Cooke.
+# Portions created by the Initial Developer are Copyright (C) 2009-2010
+# Andrew Cooke (andrew@acooke.org). All Rights Reserved.
+#
+# Alternatively, the contents of this file may be used under the terms
+# of the LGPL license (the GNU Lesser General Public License,
+# http://www.gnu.org/licenses/lgpl.html), in which case the provisions
+# of the LGPL License are applicable instead of those above.
+#
+# If you wish to allow use of your version of this file only under the
+# terms of the LGPL License and not to allow others to use your version
+# of this file under the MPL, indicate your decision by deleting the
+# provisions above and replace them with the notice and other provisions
+# required by the LGPL License.  If you do not delete the provisions
+# above, a recipient may use your version of this file under either the
+# MPL or the LGPL License.
 
 '''
 Tests for the lepl.matchers.separators module.
